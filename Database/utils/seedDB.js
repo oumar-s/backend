01e789423901cb8d8b688539ed884cadb3c03e1c
Oumar Siby <oumarsiby10@gmail.com>
2022-12-08 23:22:27 -0500

    added additional dummy-data

--- Database/utils/seedDB.js
+++ Database/utils/seedDB.js
@@ -1,23 +1,39 @@
 const { Employee, Task } = require('../models');
 
 const seedDB = async () => {
-	const dummyEmployee = await Employee.create({
+	const dummyEmployee1 = await Employee.create({
 		firstName: "Melissa",
 		lastName: "Lynch",
 		department: "Computer Science"
 	});
 	const dummyEmployee2 = await Employee.create({
+		firstName: "Oumar",
+		lastName: "Siby"
+	});
+	const dummyEmployee3 = await Employee.create({
 		firstName: "John",
-		lastName: "Jackson"
+		lastName: "Doe"
 	});
 
-	const dummyTask = await Task.create({
-		description: "create a student component",
+	const dummyTask1 = await Task.create({
+		description: "create a book component",
         priorityLevel: "high",
         completionStatus: "incomplete"
 	});
+	const dummyTask2 = await Task.create({
+		description: "create a homepage component",
+        priorityLevel: "high",
+        completionStatus: "incomplete"
+	});
+	const dummyTask3 = await Task.create({
+		description: "Make homepage responsive",
+        priorityLevel: "low",
+        completionStatus: "incomplete"
+	});
 
-	await dummyTask.setEmployee(dummyEmployee);
+	await dummyTask1.setEmployee(dummyEmployee1);
+	await dummyTask2.setEmployee(dummyEmployee2);
+	await dummyTask3.setEmployee(dummyEmployee3);
 	
 }
 
