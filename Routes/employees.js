6e50daa056b9b0b224bcbec0e0aa666d6f8f01c1
Oumar Siby <oumarsiby10@gmail.com>
2022-12-22 00:22:43 -0500

    finished backend

--- Routes/employees.js
+++ Routes/employees.js
@@ -8,11 +8,44 @@ const { Task, Employee } = require('../database/models');
 // them to the error-handling middleware (defined in app.js)
 const ash = require('express-async-handler');
 
-/** GET ALL INSTRUCTORS */
+/** GET ALL EMPLOYEES */
 router.get('/', ash(async(req, res) => {
     let employees = await Employee.findAll({include: [Task]});
     res.status(200).json(employees);
   }));
+
+/** GET EMPLOYEES BY ID*/
+router.get('/:id', ash(async(req, res) => {
+  let employee = await Employee.findByPk(req.params.id, {include: [Task]});
+  res.status(200).json(employee);
+}));
+
+// Delete instructor
+router.delete('/:id', ash(async(req, res) => {
+  await Employee.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json("Instructor deleted");
+}));
+
+// Add new instructor
+router.post('/', ash(async(req, res) => {
+  let newEmployee = await Employee.create(req.body);
+  res.status(200).json(newEmployee);
+}));
+
+// Edit instructor
+router.put('/:id', ash(async(req, res) => {
+  await Employee.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  });
+  let employee = await Employee.findByPk(req.params.id, {include: [Task]});
+  res.status(201).json(employee);
+}))
 
   //Export our router, so that it can be imported to construct our apiRouter;
   module.exports = router;
--- Routes/tasks.js
+++ Routes/tasks.js
@@ -16,5 +16,39 @@ router.get('/', ash(async(req, res) => {
     res.status(200).json(tasks);
 }));
 
+/** GET Task BY ID */
+router.get('/:id', ash(async(req, res) => {
+    let task = await Task.findByPk(req.params.id, {include: [Employee]});
+    res.status(200).json(task);
+  }));
+
+/** ADD NEW TASK */
+router.post('/', function(req, res, next) {
+    Task.create(req.body)
+      .then(createdTask => res.status(200).json(createdTask))
+      .catch(err => next(err));
+  });
+  
+  /** DELETE TASK */
+  router.delete('/:id', function(req, res, next) {
+    Task.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(() => res.status(200).json("Deleted a course!"))
+      .catch(err => next(err));
+  });
+  
+  
+/******************* EDIT *********************/
+
+router.put('/:id', ash(async(req, res) => {
+    await Task.update(req.body,
+          { where: {id: req.params.id} }
+    );
+    let task = await Task.findByPk(req.params.id);
+    res.status(201).json(task);
+  }));
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
